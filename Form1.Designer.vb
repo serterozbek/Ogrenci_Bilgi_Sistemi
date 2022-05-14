62066017cdd0645ea72d16ac9813fe7f01e64898
serterozbek <serter.ozbek@hotmail.com>
2022-05-14 22:33:12 +0300

    Notlar Excelden Datagride Çekildi

---
+++ Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 49)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(569, 326)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(460, 22)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(402, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Ders Adı"
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Ders Kodu"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Ders Adı"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Okul Numarası"
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Adı"
+        Me.Column4.Name = "Column4"
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Soyadı"
+        Me.Column5.Name = "Column5"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+End Class
---
+++ Form1.vb
@@ -0,0 +1,117 @@
+﻿Imports Microsoft.Office.Interop
+Public Class Form1
+
+    Dim excel_uyg As New Excel.Application
+    Dim exc_workbook As Excel.Workbook
+    Dim exc_sheet As Excel.Worksheet
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.Add("Algoritma")
+        ComboBox1.Items.Add(".Net'e Giriş")
+        ComboBox1.Items.Add("Veritabanı 1")
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.SelectedItem = "Algoritma" Then
+            Label1.Text = ComboBox1.SelectedItem
+            algoritmaToDataGrid()
+
+        ElseIf ComboBox1.SelectedItem = ".Net'e Giriş" Then
+            Label1.Text = ComboBox1.SelectedItem
+            netegirisToDataGrid()
+
+        ElseIf ComboBox1.SelectedItem = "Veritabanı 1" Then
+            Label1.Text = ComboBox1.SelectedItem
+            veritabaniToDataGrid()
+
+        Else messagebox.Show("Böyle bir ders kayıtlı değil.")
+        End If
+
+    End Sub
+
+    Private Sub ComboBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles ComboBox1.KeyDown
+        ' ReadOnly Combobox
+        e.SuppressKeyPress = True
+    End Sub
+
+    Public Sub algoritmaToDataGrid()
+        excel_uyg.Workbooks.Open("D:\\algoritma.xlsx")
+        exc_workbook = excel_uyg.ActiveWorkbook
+        exc_sheet = exc_workbook.ActiveSheet
+
+        Dim sayac, i As Integer
+        sayac = 0
+        i = 1
+        Do While i < 1000
+            sayac = sayac + 1
+            If exc_sheet.Cells(i, 1).Value = "" Then
+                Exit Do
+            End If
+            i = i + 1
+        Loop
+        DataGridView1.Rows.Clear()
+        For i = 1 To sayac - 2
+            DataGridView1.Rows.Add()
+            DataGridView1.Rows(i - 1).Cells(0).Value = exc_sheet.Cells(i + 1, 1).Value
+            DataGridView1.Rows(i - 1).Cells(1).Value = exc_sheet.Cells(i + 1, 2).Value
+            DataGridView1.Rows(i - 1).Cells(2).Value = exc_sheet.Cells(i + 1, 3).Value
+            DataGridView1.Rows(i - 1).Cells(3).Value = exc_sheet.Cells(i + 1, 4).Value
+            DataGridView1.Rows(i - 1).Cells(4).Value = exc_sheet.Cells(i + 1, 5).Value
+
+        Next
+    End Sub
+    Public Sub netegirisToDataGrid()
+        excel_uyg.Workbooks.Open("D:\\netegiris.xlsx")
+        exc_workbook = excel_uyg.ActiveWorkbook
+        exc_sheet = exc_workbook.ActiveSheet
+
+        Dim sayac, i As Integer
+        sayac = 0
+        i = 1
+        Do While i < 1000
+            sayac = sayac + 1
+            If exc_sheet.Cells(i, 1).Value = "" Then
+                Exit Do
+            End If
+            i = i + 1
+        Loop
+        DataGridView1.Rows.Clear()
+        For i = 1 To sayac - 2
+            DataGridView1.Rows.Add()
+            DataGridView1.Rows(i - 1).Cells(0).Value = exc_sheet.Cells(i + 1, 1).Value
+            DataGridView1.Rows(i - 1).Cells(1).Value = exc_sheet.Cells(i + 1, 2).Value
+            DataGridView1.Rows(i - 1).Cells(2).Value = exc_sheet.Cells(i + 1, 3).Value
+            DataGridView1.Rows(i - 1).Cells(3).Value = exc_sheet.Cells(i + 1, 4).Value
+            DataGridView1.Rows(i - 1).Cells(4).Value = exc_sheet.Cells(i + 1, 5).Value
+
+        Next
+    End Sub
+
+    Public Sub veritabaniToDataGrid()
+        excel_uyg.Workbooks.Open("D:\\veritabani.xlsx")
+        exc_workbook = excel_uyg.ActiveWorkbook
+        exc_sheet = exc_workbook.ActiveSheet
+
+        Dim sayac, i As Integer
+        sayac = 0
+        i = 1
+        Do While i < 1000
+            sayac = sayac + 1
+            If exc_sheet.Cells(i, 1).Value = "" Then
+                Exit Do
+            End If
+            i = i + 1
+        Loop
+
+        DataGridView1.Rows.Clear()
+        For i = 1 To sayac - 2
+            DataGridView1.Rows.Add()
+            DataGridView1.Rows(i - 1).Cells(0).Value = exc_sheet.Cells(i + 1, 1).Value
+            DataGridView1.Rows(i - 1).Cells(1).Value = exc_sheet.Cells(i + 1, 2).Value
+            DataGridView1.Rows(i - 1).Cells(2).Value = exc_sheet.Cells(i + 1, 3).Value
+            DataGridView1.Rows(i - 1).Cells(3).Value = exc_sheet.Cells(i + 1, 4).Value
+            DataGridView1.Rows(i - 1).Cells(4).Value = exc_sheet.Cells(i + 1, 5).Value
+
+        Next
+    End Sub
+End Class
